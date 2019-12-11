3bbcb99bd109a814da00c1867f2ea865b82abd8c
kamilsikora <sikorakamil26@gmail.com>
2019-12-12 00:00:32 +0100

    Createt basic physics

--- FlappySos/GameScene.swift
+++ FlappySos/GameScene.swift
@@ -9,38 +9,129 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
-    
-    var gameLogo: SKLabelNode!
-    var background: SKSpriteNode!
-    var player: Player!
+class GameScene: SKScene, SKPhysicsContactDelegate {
+        
+        var gameLogo: SKLabelNode!
+        var background: SKSpriteNode!
+        var playButton: SKSpriteNode!
+        var changeButton: SKSpriteNode!
+        var player: Player!
+        var bestScore: SKLabelNode!
+        var character: Character!
+        var ground: SKShapeNode!
+        var sky: SKShapeNode!
 
-    override func didMove(to view: SKView) {
-        player = Player()
-        initializeMenu()
-    }
+        override func didMove(to view: SKView) {
+//            creates detection of collision
+            self.physicsWorld.contactDelegate = self
+            
+            background = SKSpriteNode(imageNamed: "bg")
+            background.zPosition = 0
+            background.position = CGPoint(x: 0, y: 0)
+            self.addChild(background)
+
+            gameLogo = SKLabelNode(fontNamed: "MarkerFelt-Wide")
+            gameLogo.zPosition = 1
+            gameLogo.position = CGPoint(x: 0, y: (frame.size.height / 2) - 400)
+            gameLogo.fontSize = 100
+            gameLogo.text = "Flappy Sos"
+            gameLogo.fontColor = SKColor.white
+            self.addChild(gameLogo)
+
+            playButton = SKSpriteNode(imageNamed: "play")
+            playButton.name = "play_button"
+            playButton.zPosition = 1
+            playButton.position = CGPoint(x: -100, y: -200)
+            playButton.setScale(0.2)
+
+            self.addChild(playButton)
+
+            changeButton = SKSpriteNode(imageNamed: "change")
+            changeButton.name = "change_button"
+            changeButton.zPosition = 1
+            changeButton.position = CGPoint(x: 100, y: -200)
+            changeButton.setScale(0.2)
+            self.addChild(changeButton)
+
+            bestScore = SKLabelNode(fontNamed: "MarkerFelt-Wide")
+            bestScore.zPosition = 1
+            bestScore.position = CGPoint(x: 0, y: gameLogo.position.y - 70)
+            bestScore.fontSize = 40
+            bestScore.text = "Best Score: \(UserDefaults.standard.integer(forKey: "bestScore"))"
+            bestScore.fontColor = SKColor.white
+            self.addChild(bestScore)
+
+            player = Player(scene: self)
+        }
     
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
     
-    private func initializeMenu() {
-        background = SKSpriteNode(imageNamed: "bg")
-        background.position = CGPoint(x: 0, y: 0)
-        self.addChild(background)
-        
-        gameLogo = SKLabelNode(fontNamed: "HoeflerText-BlackItalic")
-        gameLogo.zPosition = 1
-        gameLogo.position = CGPoint(x: 0, y: (frame.size.height / 2) - 400)
-        gameLogo.fontSize = 100
-        gameLogo.text = "Flappy Sos"
-        gameLogo.fontColor = SKColor.orange
-        self.addChild(gameLogo)
-        
-        player.playerNode?.zPosition = 1
-        player.playerNode?.position = CGPoint(x: 0.0 , y: 0.0)
-        let animation = SKAction.animate(with: player.playerAnimation, timePerFrame: 0.2)
-        player.playerNode?.run(SKAction.repeatForever(animation))
-        self.addChild(player.playerNode)
-    }
+        override func update(_ currentTime: TimeInterval) {
+            
+        }
+    
+         override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //        this fun runs everytime someone touches the screen. It looks what was touched and if the play_button was touched it will start the game
+                
+            for touch in touches {
+                let location = touch.location(in: self)
+                let touchedNode = self.nodes(at: location)
+                for node in touchedNode {
+                    if node.name == "play_button" {
+                        startGame()
+                    }
+                    if node.name == "change_button" {
+                        changeCharacter()
+                    }
+                    if player.readyToPlay {
+                        player.startGame()
+                    }
+                    
+                    if player.gameStarted {
+                        player.bounce()
+                    }
+                }
+            }
+        }
+    
+//    Triggered when contact occurs
+        func didBegin(_ contact: SKPhysicsContact) {
+            player.touchedTexture()
+            
+        }
+    
+
+        private func startGame() {
+            print("start game")
+            
+            playButton.run(SKAction.scale(to: 0.25, duration: 0.05)){
+                self.playButton.run(SKAction.scale(to: 0.2, duration: 0.05))
+                self.playButton.run(SKAction.move(by: CGVector(dx: -500, dy: 0), duration: 0.5)){
+                    self.playButton.isHidden = true
+                }
+            }
+            
+            changeButton.run(SKAction.move(by: CGVector(dx: 500, dy: 0), duration: 0.5)){
+                           self.changeButton.isHidden = true
+                       }
+            
+            gameLogo.run(SKAction.move(by: CGVector(dx: 0, dy: 600), duration: 0.5)){
+                self.gameLogo.isHidden = true
+            }
+            
+            bestScore.run(SKAction.move(by: CGVector(dx: 0, dy: 600), duration: 0.5)){
+                self.bestScore.isHidden = true
+            }
+            
+           
+            player.prepareForGame()
+            ground = player.ground
+            sky = player.sky
+            self.addChild(ground)
+            self.addChild(sky)
+        }
+    
+        private func changeCharacter() {
+            print("to create...")
+        }
+    
 }
--- FlappySos/Player.swift
+++ FlappySos/Player.swift
@@ -9,13 +9,117 @@
 import SpriteKit
 
 class Player {
+    
+    enum Character {
+           case bird
+       }
+    var nextTime: Double?
+    var timeExtension: Double = 0.01
+    var scene: GameScene!
     var playerAnimation: [SKTexture]!
     var playerNode: SKSpriteNode!
+    var animation: SKAction?
+    var character: Character!
+    var readyToPlay: Bool!
+    var gameStarted: Bool!
+    var ground: SKShapeNode!
+    var sky: SKShapeNode!
     
-    init(){
-        playerAnimation = [SKTexture.init(imageNamed: "bird1"), SKTexture.init(imageNamed: "bird2"), SKTexture.init(imageNamed: "bird3"), SKTexture.init(imageNamed: "bird4")]
-        playerNode = SKSpriteNode(imageNamed: "bird1")
-        
+
+    init(scene: GameScene){
+        self.scene = scene
+        self.readyToPlay = false
+        self.gameStarted = false
+        initializePlayer()
     }
+    
+    private func initializePlayer() {
+        
+        character = Character.bird
+        
+        if character == Character.bird {
+            playerAnimation = [SKTexture.init(imageNamed: "bird1"), SKTexture.init(imageNamed: "bird2"), SKTexture.init(imageNamed: "bird3"), SKTexture.init(imageNamed: "bird4"), SKTexture.init(imageNamed: "bird1")]
+            playerNode = SKSpriteNode(imageNamed: "bird1")
+            playerNode.zPosition = 1
+            playerNode.position = CGPoint(x: 0.0 , y: 0.0)
+            playerNode.physicsBody = SKPhysicsBody(texture: playerNode.texture!, size: playerNode.texture!.size())
+            playerNode.physicsBody?.usesPreciseCollisionDetection = true
+            playerNode.physicsBody?.isDynamic = false
+            playerNode.physicsBody?.restitution = 0
+            playerNode.physicsBody?.contactTestBitMask = 0b001
+            animation = SKAction.animate(with: playerAnimation, timePerFrame: 0.1)
+            playerNode?.run(SKAction.repeatForever(animation!))
+            scene.addChild(playerNode)
+        }
+    }
+    
+//    private func update(time: Double) {
+//           if nextTime == nil {
+//               nextTime = time + timeExtension
+//           } else {
+//               if time >= nextTime! {
+//                   nextTime = time + timeExtension
+//                   updatePlayerPosition()
+//                print("update")
+//               }
+//           }
+//       }
+    func prepareForGame () {
+
+
+        var splinePointsGround = [CGPoint(x: -500, y: (-scene.frame.size.height / 2) + 30),
+                            CGPoint(x: 500, y: (-scene.frame.size.height / 2) + 30)]
+        ground = SKShapeNode(splinePoints: &splinePointsGround,
+                                         count: splinePointsGround.count)
+        ground.lineWidth = 5
+        ground.zPosition = 1
+        ground.physicsBody = SKPhysicsBody(edgeChainFrom: ground.path!)
+        ground.physicsBody?.restitution = 0
+//        ground.alpha = 0
+        ground.physicsBody?.contactTestBitMask = 0b001
+        ground.physicsBody?.isDynamic = false
+        
+      
+        var splinePointsSky = [CGPoint(x: -500, y: (scene.frame.size.height / 2) + 25),
+                                  CGPoint(x: 500, y: (scene.frame.size.height / 2) + 25)]
+        
+        sky = SKShapeNode(splinePoints: &splinePointsSky, count: splinePointsSky.count)
+        
+        sky.lineWidth = 50
+        sky.zPosition = 1
+        sky.physicsBody = SKPhysicsBody(edgeChainFrom: ground.path!)
+        sky.physicsBody?.restitution = 0
+        sky.physicsBody?.isDynamic = false
+        sky.physicsBody?.contactTestBitMask = 0b001
+        
+        animation = SKAction.animate(with: playerAnimation, timePerFrame: 0.05)
+        playerNode.removeAllActions()
+        playerNode.texture = playerAnimation[0]
+        playerNode.run(SKAction.scale(to: 0.5, duration: 0.6)) {
+            self.playerNode?.run(SKAction.move(to: CGPoint(x: -((self.scene.scene!.frame.size.width) / 4), y: 0), duration: 0.2))
+            self.readyToPlay = true
+        }
+    }
+    
+    func startGame() {
+        if readyToPlay {
+            readyToPlay = false
+            playerNode.physicsBody?.isDynamic = true
+            gameStarted = true
+        }
+    }
+    
+    func bounce() {
+        playerNode.run(SKAction.repeat(animation!, count: 1))
+        playerNode.physicsBody?.applyImpulse(CGVector(dx: 0,dy: 100))
+        //apply rotation on bounceś
+    }
+    
+    
+    
+    func touchedTexture() {
+        playerNode.physicsBody?.isDynamic = false
+    }
+    
     
 }
