20f84e943581e4334fe65268d497977fbe739ddf
kamilsikora <sikorakamil26@gmail.com>
2019-12-11 18:16:02 +0100

    Menu view is ready

--- FlappySos/GameScene.swift
+++ FlappySos/GameScene.swift
@@ -13,7 +13,10 @@ class GameScene: SKScene {
     
     var gameLogo: SKLabelNode!
     var background: SKSpriteNode!
+    var playButton: SKSpriteNode!
+    var changeButton: SKSpriteNode!
     var player: Player!
+    var bestScore: SKLabelNode!
 
     override func didMove(to view: SKView) {
         player = Player()
@@ -29,18 +32,43 @@ class GameScene: SKScene {
         background.position = CGPoint(x: 0, y: 0)
         self.addChild(background)
         
-        gameLogo = SKLabelNode(fontNamed: "HoeflerText-BlackItalic")
+        gameLogo = SKLabelNode(fontNamed: "MarkerFelt-Wide")
         gameLogo.zPosition = 1
         gameLogo.position = CGPoint(x: 0, y: (frame.size.height / 2) - 400)
         gameLogo.fontSize = 100
         gameLogo.text = "Flappy Sos"
-        gameLogo.fontColor = SKColor.orange
+        gameLogo.fontColor = SKColor.white
         self.addChild(gameLogo)
+        
+        playButton = SKSpriteNode(imageNamed: "play")
+        playButton.zPosition = 1
+        playButton.position = CGPoint(x: -100, y: -200)
+        playButton.setScale(0.2)
+        
+        self.addChild(playButton)
+        
+        changeButton = SKSpriteNode(imageNamed: "change")
+        changeButton.zPosition = 1
+        changeButton.position = CGPoint(x: 100, y: -200)
+        changeButton.setScale(0.2)
+        self.addChild(changeButton)
+        
+        bestScore = SKLabelNode(fontNamed: "MarkerFelt-Wide")
+        bestScore.zPosition = 1
+        bestScore.position = CGPoint(x: 0, y: gameLogo.position.y - 70)
+        bestScore.fontSize = 40
+        bestScore.text = "Best Score: \(UserDefaults.standard.integer(forKey: "bestScore"))"
+        bestScore.fontColor = SKColor.white
+        self.addChild(bestScore)
         
         player.playerNode?.zPosition = 1
         player.playerNode?.position = CGPoint(x: 0.0 , y: 0.0)
         let animation = SKAction.animate(with: player.playerAnimation, timePerFrame: 0.2)
         player.playerNode?.run(SKAction.repeatForever(animation))
         self.addChild(player.playerNode)
+    }
+    
+    private func startGame() {
+        
     }
 }
